0de9f8be740ba19c047ebc4cdad477c5a418be4b
elaintran <elain.ttran@gmail.com>
2019-07-09 17:02:10 -0500

    created schema and seed for mysql

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(50) NOT NULL,
+    burger_description TEXT NULL,
+    burger_price DECIMAL(10, 2) NULL,
+    devoured BOOLEAN DEFAULT 0,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, burger_description, burger_price)
+VALUES
+("Hamburger", "Bun, Beef Patty, Pickles, Onions, Ketchup, Mustard", 1.39),
+("Cheeseburger", "Bun, Beef Patty, Pickles, Onions, Ketchup, Mustard, American Cheese", 1.49),
+("Spicy Chicken Burger", "Bun, Crispy Chicken, Lettuce, Mayonnaise", 1.39);
