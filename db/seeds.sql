a965dd072731bfee0a25b80110f8e0dc92f22a73
elaintran <elain.ttran@gmail.com>
2019-07-09 21:59:16 -0500

    updated schema and seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,11 @@
-INSERT INTO burgers (burger_name, burger_ingredients)
+INSERT INTO burgers (burger_name, burger_description, burger_price)
 VALUES
-("Hamburger", "Bun, Beef Patty, Pickles, Onions, Ketchup, Mustard"),
-("Cheeseburger", "Bun, Beef Patty, Pickles, Onions, Ketchup, Mustard, American Cheese"),
-("Spicy Chicken Burger", "Bun, Crispy Chicken, Lettuce, Mayonnaise");
+("Hamburger", "The original burger starts with a 100% pure beef patty seasoned with
+just a pinch of salt and pepper, then topped with a tangy pickle, chopped onions,
+ketchup and mustard.", 1.39),
+("Cheeseburger", "Our simple, classic cheeseburger begins with a 100% pure beef patty seasoned
+with just a pinch of salt and pepper. It's topped with a tangy pickle, chopped onions,
+ketchup, mustard, and a slice of melty American cheese.", 1.59),
+("Mushroom & Swiss Burger", "Our Mushroom & Swiss Burger brings out layers of texture and flavor that come
+together perfectly, with seasoned mushrooms, creamy bistro aioli, Swiss cheese
+and crispy onion strings.", 5.49);
