5e7433dd0d8072930d9f6be72e38d5ba2a208b37
elaintran <elain.ttran@gmail.com>
2019-07-10 12:17:45 -0500

    reformatted css and wrote extra pseudocode

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,25 +5,30 @@ var connection = require("../config/connection.js");
 //home route
 router.get("/", function(req, res) {
     connection.query("SELECT * FROM burgers", function(err, data) {
-        console.log(data);
+        // console.log(data);
         res.render("index", {burgers: data})
     })
 })
 
+//create a new burger and add onto the menu
+router.post("/api/burgers", function(req, res) {
+    connection.query("INSERT INTO burgers (burger_name, burger_description, burger_price) VALUES (?, ?, ?)",
+    [req.body.name, req.body.description, req.body.price], function(err, data) {
+        res.json({burgers: data});
+    })
+})
+
+//checkout section is devoured = false
+//purchased is devoured = true
+//add another column with checkout boolean
+//first section displays all of the information from the table
+//checkout section displays the information if user is planning to checkout
+//purchased section is the devoured section
+
 module.exports = router;
 
 //NOTES
-//have tabs to flip through instead of moving submitted burgers to a section
-//on the same page
-//either route to separate page or change display to block and none on same page
-//menu is the waitlist
-//use checkboxes to select burgers from the waitlist to move them all at once
-//pop up to indicate burgers have been devoured?
-//need a devoured tab
-//maybe change devoured tab to ordered
 //or menu -> waitlist -> ordered, but too much clicking
 //need to remove and edit burger
 //edit and remove from mysql also
 //use modal for burger input
-//input sections consist of name, description, price of burger
-//test with name first, then incorporate extra information
