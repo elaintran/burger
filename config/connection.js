d15c8cf5b651593d96cd48344fb3276f6c7f5268
elaintran <elain.ttran@gmail.com>
2019-07-11 22:55:08 -0500

    commented out mysql port

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 //set up mysql connection
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    // port: 3306,
     user: "root",
     password: process.env.DB_PASSWORD,
     database: "burgers_db"
